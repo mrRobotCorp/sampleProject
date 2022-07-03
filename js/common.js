475724918389a0ddbb56e9dc4e3c53a340416ca6
YoonA <depo89055@gmail.com>
2022-07-03 23:29:31 +0900

    .
    스크롤 프로그래스바(상단 인디케이터), 스크롤탑

---
+++ js/common.js
@@ -0,0 +1,38 @@
+// ----------------- scroll indicator ------------------------
+window.addEventListener('DOMContentLoaded', function(){
+    //스크롤 이벤트 처리
+    window.addEventListener("scroll", function(event){
+        if(document.querySelector('.progressbar') != null)setProgress();
+    });
+});
+
+function setProgress() {          
+    let currY = document.documentElement.scrollTop;//스크롤한 높이
+    let totalY = document.documentElement.scrollHeight - document.documentElement.clientHeight;//전체 높이
+    let percentage = (currY / totalY) * 100;//퍼센트 값
+    document.querySelector(".progress").style.width = percentage + "%";//프로그래스바 너비 변경
+}
+
+// ------------------ scroll Top ------------------------------
+$(document).ready(function(){
+
+    var Height = $("#scrollTop").height(); 
+    $("#scrollTop").hide();
+
+    $(window).scroll(function(){ 
+        var rolling = $(this).scrollTop() >= Height; 
+        if (rolling) { 
+            $("#scrollTop").fadeIn(500).css({"position":"fixed"}); } 
+        else {
+            $("#scrollTop").fadeOut(300);
+        }
+    });
+});
+
+document.getElementById("scrollTop").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
