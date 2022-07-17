c37792bd71a986af06a16ad8b0be9b09e4773947
YoonA <depo89055@gmail.com>
2022-07-17 16:40:25 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -12,27 +12,37 @@ function setProgress() {
     let percentage = (currY / totalY) * 100;//퍼센트 값
     document.querySelector(".progress").style.width = percentage + "%";//프로그래스바 너비 변경
 }
-    
-    // ------------------ scroll Top ------------------------------
-    $(document).ready(function(){
-    
-        var Height = $("#scrollTop").height(); 
-        $("#scrollTop").hide();
-    
-        $(window).scroll(function(){ 
-            var rolling = $(this).scrollTop() >= Height; 
-            if (rolling) { 
-                $("#scrollTop").fadeIn(500).css({"position":"fixed"}); } 
-            else {
-                $("#scrollTop").fadeOut(300);
-            }
-        });
+
+// ------------------- section2 text ----------------------
+const thickness = document.querySelectorAll(".thickness");
+
+thickness.forEach(function (txt) {
+    txt.innerText = "모든 언어에 대해 아름답고 자유로운 글꼴";
+})
+
+
+
+
+// ------------------ scroll Top ------------------------------
+$(document).ready(function(){
+
+    var Height = $("#scrollTop").height(); 
+    $("#scrollTop").hide();
+
+    $(window).scroll(function(){ 
+        var rolling = $(this).scrollTop() >= Height; 
+        if (rolling) { 
+            $("#scrollTop").fadeIn(500).css({"position":"fixed"}); } 
+        else {
+            $("#scrollTop").fadeOut(300);
+        }
     });
-    
-    document.getElementById("scrollTop").onclick = function () {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
+});
+
+document.getElementById("scrollTop").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
