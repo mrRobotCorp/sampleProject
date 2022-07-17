2645b00ac722e428672404521807fe597b652297
YoonA <depo89055@gmail.com>
2022-07-17 19:47:16 +0900

    .
    .

--- js/common.js
+++ js/common.js
@@ -16,8 +16,14 @@ function setProgress() {
 // ------------------- section2 text ----------------------
 const thickness = document.querySelectorAll(".thickness");
 
-thickness.forEach(function (txt) {
-    txt.innerText = "모든 언어에 대해 아름답고 자유로운 글꼴";
+thickness.forEach(function (txt0) {
+    txt0.innerText = "모든 언어에 대해 아름답고 자유로운 글꼴";
+})
+
+const otherFont = document.querySelectorAll(".others");
+
+otherFont.forEach(function (txt1) {
+    txt1.innerText = "반듯한 고딕으로 써 본 아름다운 한글";
 })
 
 
