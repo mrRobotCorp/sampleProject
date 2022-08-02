73609aa2c0f9b99020fbf87f9c79091c52b2c59d
YoonA <depo89055@gmail.com>
2022-08-02 14:51:49 +0900

    .
    .

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,9 @@ window.onload = function() {
     var trigerMenu = document.getElementById("menu");
     var gnbMenu = document.getElementById("gnb");
 
-    trigerMenu.onclick = function(){
+    trigerMenu.onclick = function() {
         gnbMenu.style.transition = "all .5s"
+        trigerMenu.classList.toggle("open");
         this.classList.toggle("active");
         gnbMenu.classList.toggle("active");
     };
